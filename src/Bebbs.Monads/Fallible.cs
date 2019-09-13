f1f8e51158ceb573705c997684b6190c15bdf279
Ian Bebbington <github@bebbs.co.uk>
2019-09-13 11:21:46 +0100

    Initial commit with Option and Fallible

---
+++ src/Bebbs.Monads/Fallible.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Bebbs.Monads
+{
+    public static class Fallible
+    {
+        public static Fallible<T> Operation<T>(Func<T> operation)
+        {
+            try
+            {
+                return Fallible<T>.Success(operation());
+            }
+            catch (Exception exception)
+            {
+                return Fallible<T>.Fail(exception);
+            }
+        }
+
+        public static async Task<Fallible<T>> OperationAsync<T>(Func<Task<T>> operation)
+        {
+            try
+            {
+                return Fallible<T>.Success(await operation().ConfigureAwait(false));
+            }
+            catch (Exception exception)
+            {
+                return Fallible<T>.Fail(exception);
+            }
+        }
+
+        public static Fallible<TResult> Select<TSource, TResult>(this Fallible<TSource> source, Func<TSource, TResult> projection)
+        {
+            return source.IsSuccess ? Operation(() => projection(source.Value)) : Fallible<TResult>.Fail(source.Exception);
+        }
+
+        public static async Task<Fallible<TResult>> SelectAsync<TSource, TResult>(this Fallible<TSource> source, Func<TSource, Task<TResult>> projection)
+        {
+            return source.IsSuccess ? await OperationAsync(() => projection(source.Value)).ConfigureAwait(false) : Fallible<TResult>.Fail(source.Exception);
+        }
+
+        public static T ValueOrThrow<T>(this Fallible<T> source)
+        {
+            if (source.IsSuccess)
+            {
+                return source.Value;
+            }
+            else
+            {
+                throw source.Exception;
+            }
+        }
+    }
+
+    public struct Fallible<T>
+    {
+        public static Fallible<T> Success(T result)
+        {
+            return new Fallible<T>(result);
+        }
+
+        public static Fallible<T> Fail(Exception exception)
+        {
+            return new Fallible<T>(exception);
+        }
+
+        public Fallible(T value)
+        {
+            IsSuccess = true;
+            Value = value;
+            Exception = null;
+        }
+
+        public Fallible(Exception exception)
+        {
+            IsSuccess = false;
+            Value = default;
+            Exception = exception;
+        }
+
+        public bool IsSuccess { get; }
+
+        public bool IsFail => !IsSuccess;
+
+        public T Value { get; }
+
+        public Exception Exception { get; }
+    }
+}
---
+++ src/Bebbs.Monads/Option.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bebbs.Monads
+{
+    public static class Option
+    {
+        public static Option<TResult> Select<TSource,TResult>(this Option<TSource> source, Func<TSource, TResult> projection)
+        {
+            return source.IsSome ? Option<TResult>.Some(projection(source.Value)) : Option<TResult>.None;
+        }
+
+        public static async Task<Option<TResult>> SelectAsync<TSource, TResult>(this Option<TSource> source, Func<TSource, Task<TResult>> projection)
+        {
+            return source.IsSome ? Option<TResult>.Some(await projection(source.Value).ConfigureAwait(false)) : Option<TResult>.None;
+        }
+
+        public static IEnumerable<T> Collect<T>(this IEnumerable<Option<T>> source)
+        {
+            return source.Where(option => option.IsSome).Select(option => option.Value);
+        }
+
+        public static T Coalesce<T>(this Option<T> source, Func<T> value)
+        {
+            return source.IsSome ? source.Value : value();
+        }
+    }
+
+    public struct Option<T>
+    {
+        public static readonly Option<T> None = new Option<T>();
+
+        public static Option<T> Some(T value)
+        {
+            return new Option<T>(true, value);
+        }
+
+        public Option(bool isSome, T value)
+        {
+            IsSome = isSome;
+            Value = value;
+        }
+
+        public bool IsSome { get; }
+
+        public bool IsNone => !IsSome;
+
+        public T Value { get; }
+    }
+}
